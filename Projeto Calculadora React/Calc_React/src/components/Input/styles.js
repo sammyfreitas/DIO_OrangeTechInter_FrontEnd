c97936afa0f5495c8d65cb17635723466185a963
Tony Freitas <84025181+sammyfreitas@users.noreply.github.com>
2022-11-16 17:08:53 -0300

    Add files via upload

---
+++ Projeto Calculadora React/Calc_React/src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled  from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+   
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #AAAA;
+        border: 0;
+        padding: 0 10px;
+
+        float: right;
+        clear: both;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
